f199f40cf6a0696f55e6a757bec3eb8298784524
emarishkin <ee.marishkin@yandex.ru>
2024-10-02 21:09:53 +0400

    obj and Nado vernutsa1

--- Задачки js/zadacha.js
+++ Задачки js/zadacha.js
@@ -1,79 +1,49 @@
-function pow(x, n) {
-  if (n < 0) {
-    alert("Отрицательные значения 'n' не поддерживаются");
-  } else {
-    let result = 1;
+let obj = {
+    for: 1,
+    let: 2,
+    return: 3
+  };
+  
+  alert( obj.for + obj.let + obj.return );  // 6
 
-    for (let i = 0; i < n; i++) {
-      result *= x;
+//Создайте пустой объект user.
+//Добавьте свойство name со значением John.
+//Добавьте свойство surname со значением Smith.
+//Измените значение свойства name на Pete.
+//Удалите свойство name из объекта.
+
+let user = {};
+user.name = "John";
+user.surname = "Smith";
+user.name = "Pete";
+delete user.name;
+
+//Интересно очень
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+  }
+  let sum=0
+  for (let key in salaries){
+    sum += salaries[key];
+  }
+  alert(sum);
+
+  
+ //оч интересно понятно вернусь  
+  let menu = {
+    width: 200,
+    height: 300,
+    title: "My menu"
+  };
+
+  function multiplyNumeric(obj) {
+    for (let key in obj) {
+      if (typeof obj[key] == 'number') {
+        obj[key] *= 2;
+      }
     }
-
-    return result;
   }
-}
-alert(pow(2, 4))
-
-function showPrimes(n) {
-
-  for (let i = 2; i < n; i++) {
-    if (!isPrime(i)) continue;//простое число
-
-    alert(i);
-  }
-}
-
-function isPrime(n) {
-  for (let i = 2; i < n; i++) {
-    if (n % i == 0) return false;
-  }
-
-  return true;
-}
-
-describe("pow", function() {
-
-  it("возводит число в степень n", function() {
-    assert.equal(pow(2, 3), 8);
-    assert.equal(pow(3, 3), 27);
-  });
-
-});
-
-describe("pow", function() {
-
-  it("2 в степени 3 будет 8", function() {
-    assert.equal(pow(2, 3), 8);
-  });
-
-  it("3 в степени 3 будет 27", function() {
-    assert.equal(pow(3, 3), 27);
-  });
-
-});
-
-it("Возводит x в степень n", function() {
-  let x = 5;
-
-  let result = x;
-  assert.equal(pow(x, 1), result);
-
-  result *= x;
-  assert.equal(pow(x, 2), result);
-
-  result *= x;
-  assert.equal(pow(x, 3), result);
-});
-
-describe("Возводит x в степень n", function() {
-  it("5 в степени 1 будет 5", function() {
-    assert.equal(pow(5, 1), 5);
-  });
-
-  it("5 в степени 2 будет 25", function() {
-    assert.equal(pow(5, 2), 25);
-  });
-
-  it("5 в степени 3 будет 125", function() {
-    assert.equal(pow(5, 3), 125);
-  });
-});
+  multiplyNumeric(menu)
+  alert(menu.width)
